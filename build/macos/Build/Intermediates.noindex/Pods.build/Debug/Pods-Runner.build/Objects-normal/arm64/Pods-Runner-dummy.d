2876cd83c3e8d24f83371aa79332b5a19557fe40
Chris Sells <csells@sellsbrothers.com>
2024-09-09 16:09:09 -0700

    removing build folder (duh)

--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods-Runner-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/csells/Code/recipes_fat/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
-  /Users/csells/Code/recipes_fat/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m
